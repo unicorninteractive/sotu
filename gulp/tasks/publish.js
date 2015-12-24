3d27b35b6a99c3f2f7582c64f1877d6b35cb5084
Kit Cross <kit.cross@fusion.net>
2015-12-24 01:59:09 -0500

    Video and subtitles are synced

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -3,5 +3,6 @@ var execSync = require('child_process').execSync;
 var p = require('../../package.json');
 
 gulp.task('publish', ['dist'], function() {
-    execSync('gsutil -m rsync -c -r -d ./dist/ ' + p.publishUrl);
+    // execSync('git subtree push --prefix dist origin gh-pages');
+    // execSync('gsutil -m rsync -c -r -d ./dist/ ' + p.publishUrl);
 });
--- src/javascript/global.js
+++ src/javascript/global.js
@@ -1,9 +1,21 @@
-var $ = require('jquery');
-var material = require('material');
-
-// // Uncomment to use jQuery
-// $(function () {
+// var $ = require('jquery');
+// var material = require('material');
 
 
+document.addEventListener("DOMContentLoaded", function() {
 
-// });
+    var track = document.getElementById("track1");
+
+    track.addEventListener("cuechange", function() {
+        var myTrack = this.track;
+        var myCues = myTrack.activeCues;
+
+        if (myCues.length > 0) {
+            var disp = document.getElementById('cueDisplay');
+
+            disp.innerText = myCues[0].text;
+        }
+
+    }, false);
+
+}, false);
