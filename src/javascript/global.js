976f03ce0c004663bdc4e5e7500e9167cae0d39f
Kit Cross <kit.cross@fusion.net>
2016-01-08 19:26:52 -0500

    Add speech and chapters

--- src/javascript/global.js
+++ src/javascript/global.js
@@ -1,21 +1,59 @@
-var $ = require('jquery');
+var $           = require('jquery');
+var modal       = require('./modal.mdl');
+var popcorn     = require('./popcorn');
 // var material = require('material');
 
-var modal = require('./modal.mdl');
+// Data sets
+var chapters    = require('./chapters.json').chapters;
+var speech      = require('./speech.json').paragraphs;
+
+var currentChapter = 0;
 
 document.addEventListener("DOMContentLoaded", function() {
-    var track = document.getElementById("track1");
+    // Subtitle update logic
+    // var track = document.getElementById("track1");
 
-    track.addEventListener("cuechange", function() {
-        var myTrack = this.track;
-        var myCues = myTrack.activeCues;
+    // track.addEventListener("cuechange", function() {
+    //     var myTrack = this.track;
+    //     var myCues = myTrack.activeCues;
 
-        if (myCues.length > 0) {
-            var disp = document.getElementById('cueDisplay');
+    //     if (myCues.length > 0) {
+    //         var disp = document.getElementById('cueDisplay');
+    //         disp.innerText = myCues[0].text;
+    //     }
+    // }, false);
 
-            disp.innerText = myCues[0].text;
+    // Video update events
+    var popcorn = Popcorn('video');
+
+    for (var x in speech) {
+        popcorn.code({
+            start: speech[x].start,
+            end: speech[x].end,
+            onStart: function(options) {
+                $('#text-display').html(speech[x].text);
+            }
+        });
+    }
+
+    var video = document.getElementById('video');
+
+    function updateQuestions() {
+        $('#chapter-title').html(chapters[currentChapter].title);
+        $('#chapter-question-list').removeClass();
+        $('#chapter-question-list').addClass('mdl-color--' + chapters[currentChapter].color);
+        $('#chapter-question-list li').each(function(i) {
+            $(this).html(chapters[currentChapter].questions[i]);
+        });
+    }
+
+    video.ontimeupdate = function() {
+        var time = Math.floor(video.currentTime);
+        if (time > chapters[currentChapter].start) {
+            currentChapter++;
+            updateQuestions();
         }
-    }, false);
+    };
 
 }, false);
 
